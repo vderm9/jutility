09fbd2df605d0d554d2fe86c3406bcfd65e100f0
ChrisCruze <34425825+ChrisCruze@users.noreply.github.com>
2018-03-20 19:24:48 -0400

    Update jutility.js

--- jutility.js
+++ jutility.js
@@ -1,3 +1,16 @@
+//firebase initialize
+
+function firebase_initialize(){
+	var config = {
+	apiKey: "AIzaSyApJBfnH0j3TSugzEABiMFkI_tU_XXeGzg",
+	authDomain: "shippy-ac235.firebaseapp.com",
+	databaseURL: "https://shippy-ac235.firebaseio.com"
+	};
+	firebase.initializeApp(config);
+	return firebase
+}
+
+
 //function turn an array (e.g. list of dictionaries) into a list of lists because certain functions such as datatables takes an input of a list of lists
 function list_of_lists_from_array(array,keys){
 	list_of_lists = [] //this is an empty list that will be filled with sublists
@@ -11,12 +24,6 @@ function list_of_lists_from_array(array,keys){
 	return list_of_lists
 }
 
-
-function test_function () {
-
-	alert('hi')
-	
-}
 
 
 
@@ -113,6 +120,25 @@ function todoist_delete_task(todoist_api_token,task_id){
     })
 }
 
+//https://developer.todoist.com/sync/v7/#add-two-new-tasks	
+function todoist_create_task(todoist_api_token,project_id,content){
+  $.ajax({
+      type: "GET",
+      url: 'https://en.todoist.com/api/v7/sync/',
+      dataType: 'json',
+      async: false,
+      data: {
+        'token': todoist_api_token,
+        'sync_token':'*',
+        'resource_types':'["projects", "items"]',
+        'commands':'[{"type": "item_add", "uuid": "f8539c77-7fd7-4846-afad-3b201f0be8a5", "args": {"project_id": '+String(project_id)+',"content":"'+content+'" }}]'
+      }
+    })
+}
+//todoist_create_task("a14f98a6b546b044dbb84bcd8eee47fbe3788671",2178300883,"Test Task")
+//          "args": { "project_id": "24a193a7-46f7-4314-b984-27b707bd2331", "content": "Task1" } },
+
+
 function todoist_update_task(todoist_api_token,task_id,content){
   $.ajax({
       type: "GET",
@@ -162,6 +188,23 @@ function todoist_completed_tasks_with_offset(todoist_api_token,offset) {
     });
     return results.responseJSON.items
   }
+
+
+//Since todoist only lets you pull 50 tasks at a time, we're going to use a loop to get the first 50, then the second 50, then the third 50 tasks, etc. 
+//when it's pulling empty lists, it can stop 
+//we're going to use a while loop here (read more here: https://www.w3schools.com/js/js_loop_while.asp)
+function todoist_completed_tasks_all(todoist_api_token){
+  todoist_tasks_pulled = []
+  iterator = 0 
+  master_list = []
+  while (todoist_tasks_pulled.length == 50|| iterator==0) { //if todoist pulls 50 tasks, then it should try again. when it pulls less, we know that it's the last loop we need to do. since the first loop will be less than 50 tasks length, i put in or clause that is iterator is 0 which will only be when it does the first loop
+    limit_variable = 50 * iterator //this will go into the todoist completed tasks query
+    todoist_tasks_pulled = todoist_completed_tasks_with_offset(todoist_api_token,limit_variable)//this is the list of tasks 
+    master_list = master_list.concat(todoist_tasks_pulled)
+    iterator += 1; //this will be 1 in the first loop, 2 in the second loop, etc. 
+  }
+  return master_list
+}
 
 
 
@@ -426,4 +469,3 @@ function datatables_generate(array,row_replicate_func,table_id,tbody_id){
 
 
   }
-
