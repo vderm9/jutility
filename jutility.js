fb25401bcf3c05336a3fff6603d998ecafdc62a6
ChrisCruze <34425825+ChrisCruze@users.noreply.github.com>
2018-04-01 18:30:57 -0400

    Update jutility.js

--- jutility.js
+++ jutility.js
@@ -689,3 +689,10 @@ function todoist_data_dictionary(todoist_api_token){
 
 }
 
+function sum_float_convert_from_array(arr,key_name) { 
+  // returns the sum total of all values in the array
+  return _.reduce(arr, function(memo, num) { 
+    r = memo + (parseFloat(num[key_name])||0)
+    return r 
+  }, 0); 
+}
