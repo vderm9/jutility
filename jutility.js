7b0f5a99c8ba326f6b6a03e631c744470373fff5
ChrisCruze <34425825+ChrisCruze@users.noreply.github.com>
2018-04-12 23:24:37 -0400

    Update jutility.js

--- jutility.js
+++ jutility.js
@@ -889,3 +889,33 @@ function guesty_to_event_data_array(guesty_data){
 
     }
   }
+
+function output(inp) {
+    document.body.appendChild(document.createElement('pre')).innerHTML = inp;
+}
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+function json_prettify(json_input){
+var str = JSON.stringify(json_input, undefined, 4);
+//output(str);
+output(syntaxHighlight(str));
+
+}
